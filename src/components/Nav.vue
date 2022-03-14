533a065d26dcf66284a01da57cf6164dbe856af6
lehirschy <edensjunk@yahoo.com>
2022-03-14 16:50:32 -0500

    Basic Setup

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -10,34 +10,34 @@ const loggingOut = () => {
 };
 </script>
 <template>
-    <div class="text-green-200 bg-green-800">
+    <div class="text-indigo-200 bg-indigo-800">
         <div class="container flex items-center justify-between mx-auto " >
-            <h1 class="text-3xl font-thin tracking-tighter">Vue<span class="font-normal">FakeAuth</span></h1>
+            <h1 class="text-3xl font-thin tracking-tighter">Vue<span class="font-normal">RealAuth</span></h1>
             <nav>
                 <ul class="flex w-full space-x-4">
                     <router-link to="/">
-                        <li class="px-4 py-8 hover:cursor-pointer hover:bg-blue-100 hover:text-blue-800">
+                        <li class="px-4 py-8 hover:cursor-pointer hover:bg-indigo-100 hover:text-indigo-800">
                             Home 
                         </li>
                     </router-link>
                     <router-link to="/about">
-                        <li class="px-4 py-8 hover:cursor-pointer hover:bg-blue-100 hover:text-blue-800">
+                        <li class="px-4 py-8 hover:cursor-pointer hover:bg-indigo-100 hover:text-indigo-800">
                             About
                         </li>
                     </router-link>
                     <router-link v-if="!isAuthenticated" to="/login">
-                        <li class="px-4 py-8 hover:cursor-pointer hover:bg-blue-100 hover:text-blue-800">
+                        <li class="px-4 py-8 hover:cursor-pointer hover:bg-indigo-100 hover:text-indigo-800">
                             Login
                         </li>
                     </router-link>
                     <div v-else class="flex">
                         <router-link to="/secret">
-                            <li class="px-4 py-8 hover:cursor-pointer hover:bg-blue-100 hover:text-blue-800">
+                            <li class="px-4 py-8 hover:cursor-pointer hover:bg-indigo-100 hover:text-indigo-800">
                                 Secret
                             </li>
                         </router-link>
                         <button  @click="loggingOut">
-                            <li class="px-4 py-8 hover:cursor-pointer hover:bg-blue-100 hover:text-blue-800">
+                            <li class="px-4 py-8 hover:cursor-pointer hover:bg-indigo-100 hover:text-indigo-800">
                                 Logout
                             </li>
                         </button>
