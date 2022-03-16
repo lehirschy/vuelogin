1ad4b1ec8429b85c2cda2f53b9e7b44a59de12c5
lehirschy <edensjunk@yahoo.com>
2022-03-16 16:07:10 -0500

    Added Basic Validation

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,15 +1,25 @@
 <script setup>
 import {ref} from "vue";
 import { useRouter } from "vue-router"; 
+import { useForm, useField } from 'vee-validate';
+import * as yup from 'yup';
 import useAuth from "../composable/useAuth";
 import useError from "../composable/useError";
 import { promiseTimeout, useTimeout } from '@vueuse/core';
 
+const schema = yup.object({
+      username: yup.string().required().email().label("Email"),
+      password: yup.string().required().min(8).label("Password"),
+    });
+
+useForm({
+      validationSchema: schema,
+    });
+
+const { value: username, errorMessage: emailError } = useField('username');
+const { value: password, errorMessage: passwordError } = useField('password');
 
 const {isAuthenticated, login, signup, googleLogin} = useAuth();
-
-const username = ref("");
-const password = ref("");
 
 const router = useRouter();
 
@@ -45,8 +55,10 @@ const { ready, start } = useTimeout(3000, { controls: true });
        <div class="flex items-center justify-center overflow-hidden bg-gray-200 rounded-lg shadow-2xl">
          <img class="h-64" src="../assets/bglogin.png" alt="Login BG" />
          <form @submit.prevent="loggingIn" class="flex flex-col p-4 space-y-4">
-           <input type="text" class="p-2 border-2 rounded-lg" placeholder="Username" v-model="username"/>
-           <input type="password" class="p-2 border-2 rounded-lg" placeholder="Password" v-model="password"/>
+           <input name=username type="text" class="p-2 border-2 rounded-lg" placeholder="Email" v-model="username"/>
+           <span class="text-xs text-center text-red-500">{{emailError}}</span>
+           <input name=password type="password" class="p-2 border-2 rounded-lg" placeholder="Password" v-model="password"/>
+           <span class="text-xs text-center text-red-500">{{passwordError}}</span>
            <div class="flex space-x-2">
             <button type="submit" @submit.prevent="loggingIn" class="w-1/2 py-2 text-green-200 bg-green-600 rounded-lg">
               Login
