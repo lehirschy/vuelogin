c81eb6a16bdddef0580f8dec22df60b04efa38e3
lehirschy <edensjunk@yahoo.com>
2022-05-05 12:40:16 -0500

    Source Control

---
+++ src/composable/useChat.js
@@ -0,0 +1,35 @@
+import { ref } from 'vue'
+import {
+  collection,
+  query,
+  orderBy,
+  onSnapshot,
+  addDoc,
+} from 'firebase/firestore'
+
+import { db } from './useFirebase'
+import useAuth from './useAuth'
+
+const { user } = useAuth()
+
+const messages = ref([])
+
+const useChat = () => {
+  const chatCollection = collection(db, 'chat')
+
+  const chatQuery = query(chatCollection, orderBy('createdAt', 'desc'))
+
+  const unsubscribe = onSnapshot(chatQuery, querySnapshot => {
+    messages.value = []
+    querySnapshot.forEach(doc => {
+      messages.value.push({ id: doc.id, ...doc.data() })
+    })
+  })
+
+    return { messages, unsubscribe }
+}
+
+export default useChat
+
+
+
--- src/composable/useFirebase.js
+++ src/composable/useFirebase.js
@@ -1,8 +1,9 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
 
-import {getAuth} from "firebase/auth";
+import { firebaseConfig } from '../firebaseConfig'
 
-import { firebaseConfig } from "../firebaseConfig";
-
-export const firebaseApp = initializeApp(firebaseConfig);
-export const firebaseAuth = getAuth(firebaseApp);
+export const firebaseApp = initializeApp(firebaseConfig)
+export const firebaseAuth = getAuth(firebaseApp)
+export const db = getFirestore()
